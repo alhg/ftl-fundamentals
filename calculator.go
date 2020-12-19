3ae79fb837669f740cbb8fa8381fa969df9c85c4
= <alhg@users.noreply.github.com>
2020-12-18 22:44:52 -0800

    Refactor to variadic functions

--- calculator.go
+++ calculator.go
@@ -6,30 +6,47 @@ import (
 	"math"
 )
 
-// Add takes two numbers and returns the result of adding them together.
-func Add(a, b float64) float64 {
-	return a + b
-}
-
-// Subtract takes two numbers and returns the result of subtracting the first
-// from the second.
-func Subtract(a, b float64) float64 {
-	return a - b
-}
-
-// Multiple takes two numbers and returns the result of multiplying them
-// together.
-func Multiply(a, b float64) float64 {
-	return a * b
-}
-
-// Divide takes two numbers and returns the result of dividing the first
-// number by the second number.
-func Divide(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, fmt.Errorf("bad input: %f, %f (division by zero is undefined)", a, b)
+// Add takes two or more numbers and returns the result of adding them together.
+func Add(numbers ...float64) float64 {
+	sum := 0.0
+	for _, n := range numbers {
+		sum = sum + n
 	}
-	return a / b, nil
+	return sum
+}
+
+// Subtract takes two or numbers and returns the result of subtracting the first
+// from the rest of the numbers.
+func Subtract(a float64, numbers ...float64) float64 {
+	value := a
+	for _, n := range numbers {
+		value = value - n
+	}
+	return value
+}
+
+// Multiple takes two or more numbers and returns the result of multiplying them
+// together.
+func Multiply(numbers ...float64) float64 {
+	value := 1.0
+	for _, n := range numbers {
+		value = value * n
+	}
+	return value
+}
+
+// Divide takes atleast two numbers and returns the result of dividing the first
+// number by the second and repeats division with subsequent numbers.
+func Divide(a float64, numbers ...float64) (float64, error) {
+	value := a
+	for _, n := range numbers {
+		if n == 0 {
+			return 0, fmt.Errorf("bad input: %f, %v (division by zero is undefined)", a, numbers)
+		}
+		value = value / n
+	}
+
+	return value, nil
 }
 
 // Sqrt takes a number and return the square root of it.
--- calculator_test.go
+++ calculator_test.go
@@ -2,28 +2,39 @@ package calculator_test
 
 import (
 	"calculator"
+	"strconv"
+	"strings"
 	"testing"
 )
 
 type testCase struct {
 	name        string
-	a, b        float64
+	inputs      []float64
 	want        float64
 	errExpected bool
+}
+
+func getFormatVariadicInputStr(inputs []float64) string {
+	stringArr := []string{}
+	for _, i := range inputs {
+		stringArr = append(stringArr, strconv.FormatFloat(i, 'f', -1, 64))
+	}
+	return strings.Join(stringArr, ", ")
 }
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
 	testCases := []testCase{
-		{name: "Two positive numbers that sum to a positive", a: 2, b: 2, want: 4},
-		{name: "A negative & postive number that sum to positive", a: -1, b: 1, want: 0},
-		{name: "A postive and negative that sum to negative", a: 4, b: -5, want: -1},
-		{name: "Two negative numbers that sum to a negative", a: -1, b: -1, want: -2},
+		{name: "Two positive numbers that sum to a positive", inputs: []float64{2, 2}, want: 4},
+		{name: "A negative & postive number that sum to positive", inputs: []float64{-1, 1}, want: 0},
+		{name: "A postive and negative that sum to negative", inputs: []float64{4, -5}, want: -1},
+		{name: "Two negative numbers that sum to a negative", inputs: []float64{-1, -1}, want: -2},
+		{name: "Ten positive and negative numbers that sum to a positive", inputs: []float64{-2, 5, 2, -4, 3}, want: 4},
 	}
 	for _, tc := range testCases {
-		got := calculator.Add(tc.a, tc.b)
+		got := calculator.Add(tc.inputs...)
 		if tc.want != got {
-			t.Errorf("%s\nAdd(%f, %f): want %f, got %f", tc.name, tc.a, tc.b, tc.want, got)
+			t.Errorf("%s\nAdd(%s): want %f, got %f", tc.name, getFormatVariadicInputStr(tc.inputs), tc.want, got)
 		}
 	}
 }
@@ -31,15 +42,15 @@ func TestAdd(t *testing.T) {
 func TestSubtract(t *testing.T) {
 	t.Parallel()
 	testCases := []testCase{
-		{name: "Two positive numbers that subtract to a positive", a: 4, b: 2, want: 2},
-		{name: "Two positive numbers that subtract to a negative", a: 3, b: 4, want: -1},
-		{name: "Two negative numbers that subtract to a positive", a: -1, b: -2, want: 1},
-		{name: "Two negative numbers that subtract to a negative", a: -3, b: -2, want: -1},
+		{name: "Two positive numbers that subtract to a positive", inputs: []float64{4, 2}, want: 2},
+		{name: "Two positive numbers that subtract to a negative", inputs: []float64{3, 4}, want: -1},
+		{name: "Two negative numbers that subtract to a positive", inputs: []float64{-1, -2}, want: 1},
+		{name: "Two negative numbers that subtract to a negative", inputs: []float64{-3, -2}, want: -1},
 	}
 	for _, tc := range testCases {
-		got := calculator.Subtract(tc.a, tc.b)
+		got := calculator.Subtract(tc.inputs[0], tc.inputs[1:]...)
 		if tc.want != got {
-			t.Errorf("%s\nSubtract(%f, %f): want %f, got %f", tc.name, tc.a, tc.b, tc.want, got)
+			t.Errorf("%s\nSubtract(%s): want %f, got %f", tc.name, getFormatVariadicInputStr(tc.inputs), tc.want, got)
 		}
 	}
 }
@@ -47,15 +58,15 @@ func TestSubtract(t *testing.T) {
 func TestMultiply(t *testing.T) {
 	t.Parallel()
 	testCases := []testCase{
-		{name: "Two postives numbers that multiply to positive", a: 3, b: 3, want: 9},
-		{name: "Two negative numbers the multiply to positive", a: -2, b: -3, want: 6},
-		{name: "A negative and postive (in that order) that multply to negative", a: -4, b: 2, want: -8},
-		{name: "A positive and negative (in that order) that multiple to negative", a: 5, b: -1, want: -5},
+		{name: "Two postives numbers that multiply to positive", inputs: []float64{3, 3}, want: 9},
+		{name: "Two negative numbers the multiply to positive", inputs: []float64{-2, -3}, want: 6},
+		{name: "A negative and postive (in that order) that multply to negative", inputs: []float64{-4, 2}, want: -8},
+		{name: "A positive and negative (in that order) that multiple to negative", inputs: []float64{5, -1}, want: -5},
 	}
 	for _, tc := range testCases {
-		got := calculator.Multiply(tc.a, tc.b)
+		got := calculator.Multiply(tc.inputs...)
 		if tc.want != got {
-			t.Errorf("%s\nMultiply(%f, %f): want %f, got %f", tc.name, tc.a, tc.b, tc.want, got)
+			t.Errorf("%s\nMultiply(%s): want %f, got %f", tc.name, getFormatVariadicInputStr(tc.inputs), tc.want, got)
 		}
 	}
 }
@@ -63,38 +74,45 @@ func TestMultiply(t *testing.T) {
 func TestDivide(t *testing.T) {
 	t.Parallel()
 	testCases := []testCase{
-		{name: "Two postive numbers divided returns a postive", a: 4, b: 2, want: 2, errExpected: false},
-		{name: "Two negative numbers divided returns a positive", a: -4, b: -2, want: 2, errExpected: false},
-		{name: "A positive and negative numbers divided return a negative", a: 5, b: -2, want: -2.5, errExpected: false},
-		{name: "A number divided by zero that returns an error", a: 2, b: 0, want: 0, errExpected: true},
+		{name: "Two postive numbers divided returns a postive", inputs: []float64{4, 2}, want: 2, errExpected: false},
+		{name: "Two negative numbers divided returns a positive", inputs: []float64{-4, -2}, want: 2, errExpected: false},
+		{name: "A positive and negative numbers divided return a negative", inputs: []float64{5, -2}, want: -2.5, errExpected: false},
+		{name: "A number divided by zero that returns an error", inputs: []float64{2, 0}, want: 0, errExpected: true},
 	}
 	for _, tc := range testCases {
-		got, err := calculator.Divide(tc.a, tc.b)
+
+		got, err := calculator.Divide(tc.inputs[0], tc.inputs[1:]...)
 		errReceived := err != nil
 		if tc.errExpected != errReceived {
-			t.Fatalf("%s\nDivide(%f, %f): unexpected error status: %v", tc.name, tc.a, tc.b, errReceived)
+			t.Fatalf("%s\nDivide(%s): unexpected error status: %v", tc.name, getFormatVariadicInputStr(tc.inputs), errReceived)
 		}
 		if !tc.errExpected && tc.want != got {
-			t.Errorf("%s\nDivide(%f, %f): want %f, got %f", tc.name, tc.a, tc.b, tc.want, got)
+			t.Errorf("%s\nDivide(%s): want %f, got %f", tc.name, getFormatVariadicInputStr(tc.inputs), tc.want, got)
 		}
 	}
 }
 
 func TestSqrt(t *testing.T) {
 	t.Parallel()
-	testCases := []testCase{
-		{name: "A positive number returns its square root", a: 4, want: 2, errExpected: false},
-		{name: "The square root of 0 will return 0", a: 0, want: 0, errExpected: false},
-		{name: "A negative number returns an error", a: -2, want: 0, errExpected: true},
+	type singleInputTestCase struct {
+		name        string
+		input       float64
+		want        float64
+		errExpected bool
+	}
+	testCases := []singleInputTestCase{
+		{name: "A positive number returns its square root", input: 4, want: 2, errExpected: false},
+		{name: "The square root of 0 will return 0", input: 0, want: 0, errExpected: false},
+		{name: "A negative number returns an error", input: -2, want: 0, errExpected: true},
 	}
 	for _, tc := range testCases {
-		got, err := calculator.Sqrt(tc.a)
+		got, err := calculator.Sqrt(tc.input)
 		errReceived := err != nil
 		if tc.errExpected != errReceived {
-			t.Fatalf("%s\nSqrt(%f): unexpected error status: %v", tc.name, tc.a, errReceived)
+			t.Fatalf("%s\nSqrt(%f): unexpected error status: %v", tc.name, tc.input, errReceived)
 		}
 		if !tc.errExpected && tc.want != got {
-			t.Errorf("%s\nSqrt(%f): want %f, got %f", tc.name, tc.a, tc.want, got)
+			t.Errorf("%s\nSqrt(%f): want %f, got %f", tc.name, tc.input, tc.want, got)
 		}
 	}
 }
